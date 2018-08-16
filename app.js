7d8454611a5d83b7649a36f5265965d0a3ae9fc2
Drago <drago.s@vivifyideas.com>
2018-08-16 17:26:03 +0200

    myVar created inside app.js, outside the anonymous function

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 console.log('from app.js, BEFORE anonymous function\t\t'+myVar);
+var myVar = 'myVar';
 
 (function() {
-    var myVar = 'myVar';
     console.log('from app.js, INSIDE anonymous function\t\t'+myVar);
 })();
 
